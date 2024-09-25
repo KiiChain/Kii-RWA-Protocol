d688ffea633bd355c5263450231b6bdcb48619c8
GeorgeThoppil <georgethoppil2@gmail.com>
2024-09-25 11:53:40 -0400

    adding owner role functions

--- contracts/roles/src/owner_roles/msg.rs
+++ contracts/roles/src/owner_roles/msg.rs
@@ -1,15 +1,50 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
-use cosmwasm_std::Addr;
+use cosmwasm_std::{Addr, Uint128};
 
 #[cw_serde]
 pub struct InstantiateMsg {
     pub owner: Addr,
+    pub compliance_registry: Addr,
+    pub claim_topics_registry: Addr,
+    pub trusted_issuers_registry: Addr,
 }
 
 #[cw_serde]
 pub enum ExecuteMsg {
-    AddOwnerRole { role: OwnerRole, owner: Addr },
-    RemoveOwnerRole { role: OwnerRole, owner: Addr },
+    AddOwnerRole {
+        role: OwnerRole,
+        owner: Addr,
+    },
+    RemoveOwnerRole {
+        role: OwnerRole,
+        owner: Addr,
+    },
+    SetComplianceRegistry {
+        compliance_registry: Addr,
+    },
+    SetClaimTopicsRegistry {
+        claim_topic_registry: Addr,
+    },
+    SetTrustedIssuersRegistry {
+        trusted_issuer_registry: Addr,
+    },
+    AddTrustedIssuer {
+        issuer: Addr,
+        claim_topics: Vec<Uint128>,
+    },
+    RemoveTrustedIssuer {
+        issuer: Addr,
+    },
+    UpdateIssuerClaimTopics {
+        issuer: Addr,
+        claim_topics: Vec<Uint128>,
+    },
+    AddClaimTopic {
+        claim_topic: Uint128,
+    },
+    RemoveClaimTopic {
+        claim_topic: Uint128,
+    },
 }
 
 #[cw_serde]
--- contracts/roles/src/owner_roles/state.rs
+++ contracts/roles/src/owner_roles/state.rs
@@ -4,3 +4,6 @@ use cw_storage_plus::Item;
 
 pub const OWNER_ROLES: RoleManagement = RoleManagement::new("owner_roles");
 pub const OWNER: Item<Addr> = Item::new("owner");
+pub const COMPLIANCE_REGISTRY: Item<Addr> = Item::new("compliance");
+pub const CLAIM_TOPICS_REGISTRY: Item<Addr> = Item::new("claim_topic");
+pub const TRUSTED_ISSUERS_REGISTRY: Item<Addr> = Item::new("issuer");
