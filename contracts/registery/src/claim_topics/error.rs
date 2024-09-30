027c76fe927ff6ba9ef02500394250e059a34145
GeorgeThoppil <georgethoppil2@gmail.com>
2024-09-30 10:51:49 -0400

    adding claim topics contract

--- contracts/registery/src/claim_topics/error.rs
+++ contracts/registery/src/claim_topics/error.rs
@@ -8,6 +8,11 @@ pub enum ContractError {
 
     #[error("Unauthorized")]
     Unauthorized {},
+
+    #[error("ClaimTopicsExists")]
+    ClaimTopicsExists {},
+    #[error("ClaimTopicsNotFound")]
+    ClaimTopicsNotFound {},
     // Add any other custom errors you like here.
     // Look at https://docs.rs/thiserror/1.0.21/thiserror/ for details.
 }
