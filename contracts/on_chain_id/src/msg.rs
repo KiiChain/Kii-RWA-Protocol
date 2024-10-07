849f12d65be95431f6c518331902a771fe84b544
vcart <112975047+wayzeek@users.noreply.github.com>
2024-10-07 14:59:19 -0400

    Revert "Kiic 3"

--- contracts/on_chain_id/src/msg.rs
+++ contracts/on_chain_id/src/msg.rs
@@ -1,56 +1,11 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
-use cosmwasm_std::Binary;
-use crate::state::{Key, Claim};
 
 #[cw_serde]
-pub struct InstantiateMsg {
-    pub owner: String,
-}
+pub struct InstantiateMsg {}
 
 #[cw_serde]
-pub enum ExecuteMsg {
-    AddKey {
-        key_owner: String,
-        key_type: String,
-    },
-    RevokeKey {
-        key_owner: String,
-        key_type: String,
-    },
-    AddClaim {
-        claim: Claim,
-        public_key: Binary,
-    },
-    RemoveClaim {
-        claim_id: String,
-    },
-}
+pub enum ExecuteMsg {}
 
 #[cw_serde]
 #[derive(QueryResponses)]
-pub enum QueryMsg {
-    #[returns(Key)]
-    GetKey {
-        key_owner: String,
-        key_type: String,
-    },
-    #[returns(Claim)]
-    GetClaim {
-        claim_id: String,
-    },
-    #[returns(Vec<String>)]
-    GetClaimIdsByTopic {
-        topic: String,
-    },
-    #[returns(Vec<Claim>)]
-    GetClaimsByIssuer {
-        issuer: String,
-    },
-    #[returns(bool)]
-    VerifyClaim {
-        claim_id: String,
-        trusted_issuers_registry: String,
-    },
-    #[returns(String)]
-    GetOwner {},
-}
+pub enum QueryMsg {}
