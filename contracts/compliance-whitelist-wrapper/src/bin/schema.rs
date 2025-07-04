3659c19a2ec9517764b52e8c524927a87bb77ef4
Thales Zirbel <thales.zirbel@brickabode.com>
2025-07-04 16:07:54 -0300

    chore: rename contract

--- contracts/compliance-whitelist-wrapper/src/bin/schema.rs
+++ contracts/compliance-whitelist-wrapper/src/bin/schema.rs
@@ -1,4 +1,4 @@
-use compliance_wrapper::msg::{ExecuteMsg, InstantiateMsg};
+use compliance_whitelist_wrapper::msg::{ExecuteMsg, InstantiateMsg};
 use cosmwasm_schema::write_api;
 use utils::compliance::QueryMsg;
 
